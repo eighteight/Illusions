bf3a424c8e1ea87eaeb4c910b2694360a70a1e23
Eight IO <bo-ba@mail.ru>
2015-05-21 16:52:48 -0400

    working on lines

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 (function(window) {
     var $allSketches = $(".all-sketches");
-    var $navbarElement = $(".nav");
 
     function initializeSketchAndNav(sketchName) {
         var sketch = getSketch(sketchName);
+        if(typeof sketch === 'undefined'){
+            return;
+        } 
         var $sketchElement = initializeSketch(sketch, $allSketches);
 
-        $sketchElement.prepend("<a class='new-window' target='_blank' href='" + sketchName + "'><i class='fa fa-external-link'></i></a>");
-
-        // add sketch element to nav
-        var $navElement = $('<li></li>');
-        $navElement.text(sketch.id)
-                   .click(function () {
-                       $('body').animate({ scrollTop: $sketchElement.offset().top - 55 }, 600);
-                   })
-                   .appendTo($navbarElement);
     }
 
     initializeSketchAndNav("line");
